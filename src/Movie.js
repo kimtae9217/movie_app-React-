508b3cb01744183a9dcc2c209c7a600420df079b
taewon <kimtae9217@gmail.com>
2021-01-24 12:40:16 +0900

    Styling the Movies

--- src/Movie.js
+++ src/Movie.js
@@ -2,15 +2,24 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({ year, title, summary, poster }) {
-    return <div class="movies__movie">
-        <img src={poster} alt={title} title={title} />
-        <div>
-            <h3 class="movie__title">{title}</h3>
-            <h5 class="movie__year">{year}</h5>
-            <p class="movie__summary">{summary}</p>
+function Movie({ year, title, summary, poster, genres }) {
+    return (
+        <div className="movie">
+            <img src={poster} alt={title} title={title} />
+            <div>
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="movie__genres">
+                    {genres.map((genre, index) => (
+                        <li key={index} className="genres__genre">
+                            {genre}
+                        </li>
+                    ))}
+                </ul>
+                <p className="movie__summary">{summary.slice(0, 180)}...</p>
+            </div>
         </div>
-    </div>
+    );
 }
 
 Movie.propTypes = {
@@ -18,7 +27,9 @@ Movie.propTypes = {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
+    
 }
 
 export default Movie;
