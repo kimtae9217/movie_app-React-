2f1ef4e02555cc69e7031eee090a190cacb094d3
taewon <kimtae9217@gmail.com>
2021-01-27 22:19:42 +0900

    Sharing props between routes

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,24 +1,38 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import "./Movie.css";
 
 function Movie({ year, title, summary, poster, genres }) {
     return (
-        <div className="movie">
-            <img src={poster} alt={title} title={title} />
-            <div>
-                <h3 className="movie__title">{title}</h3>
-                <h5 className="movie__year">{year}</h5>
-                <ul className="movie__genres">
-                    {genres.map((genre, index) => (
-                        <li key={index} className="genres__genre">
-                            {genre}
-                        </li>
-                    ))}
-                </ul>
-                <p className="movie__summary">{summary.slice(0, 180)}...</p>
+        <Link
+        to={{
+            pathname: "/movie-detail",
+            state: {
+                year,
+                title,
+                summary,
+                poster,
+                genres
+            }
+        }}
+        >
+            <div className="movie">
+                <img src={poster} alt={title} title={title} />
+                <div>
+                    <h3 className="movie__title">{title}</h3>
+                    <h5 className="movie__year">{year}</h5>
+                    <ul className="movie__genres">
+                        {genres.map((genre, index) => (
+                            <li key={index} className="genres__genre">
+                                {genre}
+                            </li>
+                        ))}
+                    </ul>
+                    <p className="movie__summary">{summary.slice(0, 180)}...</p>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 }
 
@@ -29,7 +43,7 @@ Movie.propTypes = {
     summary: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,
     genres: PropTypes.arrayOf(PropTypes.string).isRequired
-    
+
 }
 
 export default Movie;
